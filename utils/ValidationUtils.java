a85b1cfd897d5154ccf6d1623fe60c1ceccef9d8
alfreddey <126983853+alfreddey@users.noreply.github.com>
2025-12-07 10:34:04 -0200

    Add InvalidProjectIDException

--- utils/ValidationUtils.java
+++ utils/ValidationUtils.java
@@ -1,21 +1,23 @@
 package utils;
 
+import utils.exceptions.InvalidProjectIDException;
+
 public class ValidationUtils {
-    public static void validateProjectID(String projectID) throws Exception {
+    public static void validateProjectID(String projectID) throws InvalidProjectIDException {
         final int MAX_PROJECT_ID_LENGTH = 4;
         int projectIdLength = projectID.length();
 
         if ((projectID.charAt(0) != 'P'
                 && projectID.charAt(0) != 'p')
                 || projectIdLength != MAX_PROJECT_ID_LENGTH) {
-            throw new Exception("Invalid input, please enter a valid ID (e.g., P001).");
+            throw new InvalidProjectIDException("Invalid input, please enter a valid ID (e.g., P001).");
         }
 
         for (int i = 1; i < projectIdLength; i++) {
             var character = projectID.charAt(i);
 
             if (!Character.isDigit(character)) {
-                throw new Exception("Invalid input, please enter a valid ID (e.g., P001).");
+                throw new InvalidProjectIDException("Invalid input, please enter a valid ID (e.g., P001).");
             }
         }
     }
---
+++ utils/exceptions/InvalidProjectIDException.java
@@ -0,0 +1,7 @@
+package utils.exceptions;
+
+public class InvalidProjectIDException extends Exception {
+    public InvalidProjectIDException(String message) {
+        super(message);
+    }
+}
