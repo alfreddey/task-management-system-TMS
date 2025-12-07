bbaf135728fb3ed805de7fdb004ad095b7fb613f
alfreddey <126983853+alfreddey@users.noreply.github.com>
2025-12-07 10:45:37 -0200

    Refactor: Throw InvalidTaskIDException

--- utils/ValidationUtils.java
+++ utils/ValidationUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import utils.exceptions.InvalidProjectIDException;
+import utils.exceptions.InvalidTaskIDException;
 
 public class ValidationUtils {
     public static void validateProjectID(String projectID) throws InvalidProjectIDException {
@@ -22,21 +23,21 @@ public class ValidationUtils {
         }
     }
 
-    public static void validateTaskID(String taskID) throws Exception {
+    public static void validateTaskID(String taskID) throws InvalidTaskIDException {
         final int MAX_PROJECT_ID_LENGTH = 4;
         int projectIdLength = taskID.length();
 
         if ((taskID.charAt(0) != 'T'
                 && taskID.charAt(0) != 't')
                 || projectIdLength != MAX_PROJECT_ID_LENGTH) {
-            throw new Exception("Invalid input, please enter a valid ID (e.g., T001).");
+            throw new InvalidTaskIDException("Invalid input, please enter a valid ID (e.g., T001).");
         }
 
         for (int i = 1; i < projectIdLength; i++) {
             var character = taskID.charAt(i);
 
             if (!Character.isDigit(character)) {
-                throw new Exception("Invalid input, please enter a valid ID (e.g., T001).");
+                throw new InvalidTaskIDException("Invalid input, please enter a valid ID (e.g., T001).");
             }
         }
     }
