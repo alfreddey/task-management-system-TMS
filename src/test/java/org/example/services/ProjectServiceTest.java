8cd2325c27e9d24d17bda01a345dbd506414cc37
alfreddey <alfrednelly246@gmail.com>
2025-12-09 10:16:35 +0000

    Add test cases to ValidationUtils, ProjectService, Task test

--- src/test/java/org/example/services/ProjectServiceTest.java
+++ src/test/java/org/example/services/ProjectServiceTest.java
@@ -1,4 +1,57 @@
+package org.example.services;
+
+import org.example.models.Project;
+import org.example.models.SoftwareProject;
+import org.example.utils.exceptions.ProjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProjectServiceTest {
-  
+    ProjectService projectService;
+
+    @BeforeEach
+    @Test
+    void setUp() {
+        projectService = ProjectService.getService();
+    }
+
+    @Test
+    void testAddProject() {
+        Project softwareProject = new SoftwareProject("IR Software", "sdesc", 12, 43);
+
+
+        assertDoesNotThrow(() -> {
+            projectService.addProject(softwareProject);
+            projectService.getProjectById(softwareProject.getId());
+        });
+    }
+
+    @Test
+    void testGetProjectById() {
+        Project softwareProject = new SoftwareProject("Projectssdf", "sdesc", 102, 43);
+
+
+        assertDoesNotThrow(() -> {
+            projectService.addProject(softwareProject);
+            projectService.getProjectById(softwareProject.getId());
+        });
+    }
+
+    @Test
+    void testAddSoftwareProject() {
+        assertDoesNotThrow(() -> {
+            projectService.addSoftwareProject("Projectssdf", "sdesc", 102, 43);
+            projectService.getProjectById("P001");
+        });
+    }
+
+    @Test
+    void testAddHardwareProject() {
+        assertDoesNotThrow(() -> {
+            projectService.addHardwareProject("Projectssdf", "sdesc", 102, 43, 2342.3);
+            projectService.getProjectById("P001");
+        });
+    }
 }
--- src/test/java/org/example/utils/ValidationUtilsTest.java
+++ src/test/java/org/example/utils/ValidationUtilsTest.java
@@ -1,4 +1,45 @@
+package org.example.utils;
+
+import org.example.models.Project;
+import org.example.models.SoftwareProject;
+import org.example.models.Task;
+import org.example.models.TaskStatus;
+import org.example.utils.exceptions.InvalidProjectIDException;
+import org.example.utils.exceptions.InvalidTaskIDException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ValidationUtilsTest {
-  
+    Project project;
+    Task task;
+
+    @BeforeEach
+    void setUp() {
+        project = new SoftwareProject("My name", "snd", 12, 344.5);
+        task = new Task("adfa", TaskStatus.IN_PROGRESS, "P001");
+    }
+
+    @Test
+    void validateProjectID() {
+        assertDoesNotThrow(() -> {
+            ValidationUtils.validateProjectID("P001");
+        });
+
+        assertThrows(InvalidProjectIDException.class, () -> {
+            ValidationUtils.validateProjectID("P00X");
+        }, "Valid project ID was passed as an argument");
+    }
+
+    @Test
+    void validateTaskID() {
+        assertDoesNotThrow(() -> {
+            ValidationUtils.validateTaskID("T001");
+        });
+
+        assertThrows(InvalidTaskIDException.class, () -> {
+            ValidationUtils.validateTaskID("T00X");
+        }, "Valid task ID was passed as an argument");
+    }
 }
