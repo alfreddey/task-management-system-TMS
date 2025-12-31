9c81d4be0a908b746dead8b730dbd70a0e72bee0
alfreddey <lilscar246@gmail.com>
2025-12-31 17:16:17 +0000

    Update validation utils to use regex

--- src/main/java/org/example/utils/ValidationUtils.java
+++ src/main/java/org/example/utils/ValidationUtils.java
@@ -3,42 +3,33 @@ package org.example.utils;
 import org.example.utils.exceptions.InvalidProjectIDException;
 import org.example.utils.exceptions.InvalidTaskIDException;
 
+import java.util.regex.Pattern;
+
 public class ValidationUtils {
-    public static void validateProjectID(String projectID) throws InvalidProjectIDException {
-        final int MAX_PROJECT_ID_LENGTH = 4;
-        int projectIdLength = projectID.length();
+    public static void validateEmail(String email) throws Exception {
+        var pattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", Pattern.CASE_INSENSITIVE);
+        var matcher = pattern.matcher(email);
 
-        if ((projectID.charAt(0) != 'P'
-                && projectID.charAt(0) != 'p')
-                || projectIdLength != MAX_PROJECT_ID_LENGTH) {
-            throw new InvalidProjectIDException("Invalid input, please enter a valid ID (e.g., P001).");
+        if (!matcher.find()) {
+            throw new Exception("Invalid email format. Please enter a valid email (e.g. al@gmail.com).");
         }
+    }
 
-        for (int i = 1; i < projectIdLength; i++) {
-            var character = projectID.charAt(i);
+    public static void validateProjectID(String projectID) throws InvalidProjectIDException {
+        var pattern = Pattern.compile("^P\\d{3}\\b", Pattern.CASE_INSENSITIVE);
+        var matcher = pattern.matcher(projectID);
 
-            if (!Character.isDigit(character)) {
-                throw new InvalidProjectIDException("Invalid input, please enter a valid ID (e.g., P001).");
-            }
+        if (!matcher.find()) {
+            throw  new InvalidProjectIDException("Invalid input, please enter a valid ID (e.g., P001).");
         }
     }
 
     public static void validateTaskID(String taskID) throws InvalidTaskIDException {
-        final int MAX_PROJECT_ID_LENGTH = 4;
-        int projectIdLength = taskID.length();
+        var pattern = Pattern.compile("^T\\d{3}\\b", Pattern.CASE_INSENSITIVE);
+        var matcher = pattern.matcher(taskID);
 
-        if ((taskID.charAt(0) != 'T'
-                && taskID.charAt(0) != 't')
-                || projectIdLength != MAX_PROJECT_ID_LENGTH) {
-            throw new InvalidTaskIDException("Invalid input, please enter a valid ID (e.g., T001).");
-        }
-
-        for (int i = 1; i < projectIdLength; i++) {
-            var character = taskID.charAt(i);
-
-            if (!Character.isDigit(character)) {
-                throw new InvalidTaskIDException("Invalid input, please enter a valid ID (e.g., T001).");
-            }
+        if (!matcher.find()) {
+            throw  new InvalidTaskIDException("Invalid input, please enter a valid ID (e.g., T001).");
         }
     }
 }
